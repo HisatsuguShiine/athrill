7019c050a2880ce420cbefc41786a17d9c8876e0
HisatsuguShiine <atoh214@gmail.com>
2020-08-16 23:05:08 +0900

    warnig fix

--- trunk/src/lib/dwarf/data_type/elf_dwarf_struct_type.c
+++ trunk/src/lib/dwarf/data_type/elf_dwarf_struct_type.c
@@ -120,6 +120,7 @@ static void elf_dwarf_build_struct_method(DwarfDataStructType *obj, ElfDwarfDieT
 		case DW_AT_const_value:
 		case DW_AT_encoding:
 		case DW_AT_unknown_0x2117:
+		case DW_AT_unknown_0x2116:
 			break;
 		default:
 			printf("attr_type=0x%x\n", attr_type);
